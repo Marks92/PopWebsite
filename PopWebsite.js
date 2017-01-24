dc23527ca9019d8c7bcc5cfc41aa3635da3c6390
Mark Schuberth <Mark Schuberth>
2017-01-24 07:31:27 -0500

    First commit

---
+++ PopWebsite.js
@@ -0,0 +1,26 @@
+var main = function() {
+  /* This will push the body of the menu 285px to the right */
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+
+  /* This will push the menu back to 0 pixels */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+};
+
+
+$(document).ready(main);
